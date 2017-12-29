dfd3da21c109e06eaef42a7078b9d18d4aefc4fb
roberto <rob-mg@hotmail.com>
2017-12-29 14:40:19 -0200

    ajustes

--- src/select-custom/select.custom.module.ts
+++ src/select-custom/select.custom.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { SelectCustomComponent } from './select.custom.component';
 import { CommonModule } from '@angular/common';
-import { NgPipesModule } from 'ngx-pipes';
 import { Ng2FilterPipeModule } from 'ng2-filter-pipe';
 import { FormsModule } from '@angular/forms';
 
